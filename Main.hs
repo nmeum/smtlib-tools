fd632f613fc0cd03757fe795f352a3dd2e83ade8
Sören Tempel <tempel@ibr.cs.tu-bs.de>
2026-04-15 06:23:30 +0200

    Support incremental queries

--- Main.hs
+++ Main.hs
@@ -82,6 +82,10 @@ collectCmd (SMT.List [SMT.Atom "check-sat-assuming", SMT.List assumptions])
   = collectName "check-sat-assuming" >> forM_ assumptions collectExpr
 collectCmd (SMT.List ((SMT.Atom "set-logic") : _)) = pure ()
 collectCmd (SMT.List ((SMT.Atom "declare-fun") : _)) = pure ()
+collectCmd (SMT.List (SMT.Atom "check-sat":_)) = pure ()
+collectCmd (SMT.List (SMT.Atom "pop":_)) = pure ()
+collectCmd (SMT.List (SMT.Atom "push":_)) = pure ()
+collectCmd (SMT.List [SMT.Atom "assert", expr]) = collectExpr expr
 collectCmd cmd = error $ "collectCmd: Unknown command '" ++ show cmd ++ "'"
 
 collect :: [SMT.SExpr] -> State Stats ()
